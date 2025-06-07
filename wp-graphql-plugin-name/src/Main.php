e5fec0025b364874fbca2907c1308fdcc1c6d5c1
Dovid Levine <david@axepress.dev>
2025-06-07 04:52:57 +0300

    chore: cleanup and version bump

--- wp-graphql-plugin-name/src/Main.php
+++ wp-graphql-plugin-name/src/Main.php
@@ -30,6 +30,9 @@ if ( ! class_exists( 'WPGraphQL\PluginName\Main' ) ) :
 			if ( ! isset( self::$instance ) || ! ( is_a( self::$instance, self::class ) ) ) {
 				// You cant test a singleton.
 				// @codeCoverageIgnoreStart .
+				if ( ! function_exists( 'is_plugin_active' ) ) {
+					require_once ABSPATH . 'wp-admin/includes/plugin.php'; // @phpstan-ignore requireOnce.fileNotFound
+				}
 				self::$instance = new self();
 				self::$instance->includes();
 				self::$instance->setup();
